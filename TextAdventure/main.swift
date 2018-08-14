7f75d4e17642573543d1e0a71eaadd725ec08855
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-08-14 12:06:43 -0500

    Initial Commit

---
+++ TextAdventure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TextAdventure
+//
+//  Created by Charles Martin Reed on 8/14/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
